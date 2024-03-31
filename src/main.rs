0d48ae1e0b02cfb5315113c0c232ff467dad8570
tony <tchow@tutamail.com>
2024-03-30 23:46:41 -0400

    create workspace abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,84 @@
 use std::env;
 use std::fs;
-use std::path;
 use std::path::PathBuf;
 use std::process;
 
-fn init(mut path_buf: PathBuf) {
+fn initialize_repo_directory(mut path_buf: PathBuf) -> std::io::Result<()> {
     path_buf.push(".git");
     let dirs = ["objects", "refs"];
     for dir in dirs.into_iter() {
         path_buf.push(dir);
-        let _ = fs::create_dir_all(&path_buf);
+        fs::create_dir_all(&path_buf)?;
         path_buf.pop();
     }
+    Ok(())
+}
+
+fn init(args: Vec<String>) -> std::io::Result<()> {
+    let dir = args.get(2).unwrap_or_else(|| &"./".to_string());
+    let path = fs::canonicalize(*dir).or_else(|_| {
+        fs::create_dir_all(dir)?;
+        dbg!("creating new directory {:?}", dir);
+        Ok(PathBuf::from(dir))
+    })?;
+    println!("Initialized empty Git repository in {}", path.display());
+    initialize_repo_directory(path)?;
+    Ok(())
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: {} <command> [<directory>]", args[0]);
-        process::exit(1);
-    }
-    let cmd = args[1].as_str();
-    match cmd {
-        "init" => {
-            let cwd = "./".to_string();
-            let dir = args.get(2).unwrap_or(&cwd);
-            let path_buf_result = fs::canonicalize(dir);
-            match path_buf_result {
-                Ok(path_buf) => {
-                    init(path_buf);
-                }
-                Err(_) => {
-                    let path = path::Path::new(dir);
-                    let _ = fs::create_dir_all(&path);
-                    let path_buf = path.into();
-                    init(path_buf);
-                }
-            }
+    let args: Vec<String> = env::args().collect();
+    let cmd = args.get(1).unwrap_or_else(|| &"./".to_string());
+    match Command::from(*cmd) {
+        Command::Init => {
+            todo!()
         }
-        _ => todo!("unimplemented command {}", cmd),
+        Command::Commit => {
+            let workspace = Workspace::new("./");
+            todo!()
+        }
+        _ => {
+            eprintln!("Usage: {} <command> [<directory>]", args[0]);
+            process::exit(1);
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Command {
+    Init,
+    Commit,
+}
+
+impl From<&str> for Command {
+    fn from(s: &str) -> Self {
+        match s {
+            "init" => Command::Init,
+            "commit" => Command::Commit,
+        }
+    }
+}
+
+impl From<String> for Command {
+    fn from(s: String) -> Self {
+        match &s[..] {
+            "init" => Command::Init,
+            "commit" => Command::Commit,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Workspace {
+    ignore: [&'static str; 2],
+    path_name: String,
+}
+
+impl Workspace {
+    fn new(path_name: &str) -> Self {
+        return Workspace {
+            ignore: [".", ".."],
+            path_name: path_name.to_string(),
+        };
     }
 }
